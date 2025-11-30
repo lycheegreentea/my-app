bdd75538ff894db6f6b860e7882dab147728ca75
lycheegreentea <koala269@outlook.com>
2025-11-30 09:53:45 -0500

    imperativehandle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React, { useImperativeHandle } from "react";
 import Webcam from "react-webcam";
 import ReactDOM from 'react-dom/client';
 import { useState } from 'react';
@@ -13,6 +13,11 @@ var filter = "color";
 
 
 function App() {
+  const [imgSrc1, setImgSrc1] = React.useState(null);
+  const [imgSrc2, setImgSrc2] = React.useState(null);
+  const [imgSrc3, setImgSrc3] = React.useState(null);
+  //creates ref
+  const captureRef = React.useRef(null); 
   return (
     <div class="App">
         <div class="toprow">
@@ -22,36 +27,47 @@ function App() {
         <div class="midsection">
           <div class="camframe">
             <img class="photoframe" src = {frame}></img>
-            <WebcamCapture > </WebcamCapture>
+            <WebcamCapture ref={captureRef} setImgSrc1={setImgSrc1} setImgSrc2={setImgSrc2} setImgSrc3={setImgSrc3} > </WebcamCapture>
+            <p id="countingdown">3</p>
           </div>
-
+          <div class="photo-strip">
+            <div class="photo-slot"><img id = "photo1" src = {imgSrc1} /></div>
+            <div class="photo-slot"><img id = "photo2" src = {imgSrc2} /></div>
+            <div class="photo-slot"><img id = "photo3" src = {imgSrc3} /></div>
+          </div>
         </div>
-        <button onClick={updateFilter("bw")}>b&w</button>
-        <button onClick={updateFilter("color")}>color</button>
-        <p id="countingdown">3</p>
+        <div class="bottom">
+          <button class="startbutton" onClick={() => captureRef.current.startCapture()}>Start!</button>
+          <button class ="bw" onClick={updateFilter("bw")}>b&w</button>
+          <button class="color" onClick={updateFilter("color")}>color</button>
+        </div>
+        
     </div>
   );
 }
+
 function updateFilter(fil){
   filter = fil;
 }
 
-const WebcamCapture = () => {
+const WebcamCapture = React.forwardRef((props, ref) => {
+  //ref is passed to child so parent can access
   const webcamRef = React.useRef(null);
-  const [imgSrc1, setImgSrc1] = React.useState(null);
-  const [imgSrc2, setImgSrc2] = React.useState(null);
-  const [imgSrc3, setImgSrc3] = React.useState(null);
-  const [imgSrc4, setImgSrc4] = React.useState(null);
+  React.useImperativeHandle(ref, () => ({
+    //can access startCapture from outside
+    startCapture() {
+      gogogo();
+    }
+  }));
 
     async function gogogo(){
       await countDown();
-      setImgSrc1(webcamRef.current.getScreenshot());
+      props.setImgSrc1(webcamRef.current.getScreenshot());
       await countDown();
-      setImgSrc2(webcamRef.current.getScreenshot());
+      props.setImgSrc2(webcamRef.current.getScreenshot());
       await countDown();
-      setImgSrc3(webcamRef.current.getScreenshot());
-      await countDown();
-      setImgSrc4(webcamRef.current.getScreenshot());
+      props.setImgSrc3(webcamRef.current.getScreenshot());
+      
 
     }
 
@@ -86,33 +102,13 @@ const WebcamCapture = () => {
         screenshotFormat="image/jpeg"
       />
       </div>
-      <div id="photo-strip">
-
-
-        <div class="photo-slot">
-          <img id = "photo1" src = {imgSrc1} />
-        </div>
-
-        <div class="photo-slot">
-        <img id = "photo2" src = {imgSrc2} />
-        </div>
-
-        <div class="photo-slot">
-        <img id = "photo3" src = {imgSrc3} />
-        </div>
-
-        <div class="photo-slot">
-        <img id = "photo4" src = {imgSrc4} />
-        </div>
-
+      
       </div>
-      </div>
-      <button class="startbutton" onClick={gogogo}>Start!</button>
       
     </>
   );
 
-}
+});
 //321 go 
 
 
